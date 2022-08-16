08786b80fbcccdd29a0a1749e88e293afbbafb1a
“00Amit0” <amit836961@gmail.com>
2022-08-16 14:26:59 +0530

    Added func. Calculate Wage till a specific time

--- Emp_Wage.py
+++ Emp_Wage.py
@@ -10,9 +10,10 @@ print("\n")
 
 class EmployeeWage:
 
-    def __init__(self, wage_per_hour, monthly_working_day):
+    def __init__(self, wage_per_hour, monthly_working_day, total_working_hour):
         self.wage_per_hour = wage_per_hour
         self.monthly_working_day = monthly_working_day
+        self.total_working_hour = total_working_hour
 
     def attendance(self, check):
         """
@@ -51,10 +52,15 @@ class EmployeeWage:
         try:
             total_wage = 0
             no_of_working_days = 0
-            while no_of_working_days < self.monthly_working_day:
+            working_hours = 0
+            while no_of_working_days < self.monthly_working_day and working_hours < self.total_working_hour:
                 no_of_working_days += 1
                 check = random.randint(0, 2)
+                print("\n")
                 daily_work_hour = self.attendance(check)
+                print(f" The working days is : {no_of_working_days}")
+                working_hours += daily_work_hour
+                print(f" The working hours is : {working_hours}")
                 daily_wage = self.wage_per_hour * daily_work_hour
                 print(f" The daily wage is : {daily_wage}")
                 total_wage += daily_wage
@@ -67,7 +73,7 @@ class EmployeeWage:
 
 if __name__ == "__main__":
     try:
-        emp = EmployeeWage(20, 20)
+        emp = EmployeeWage(20, 20, 100)
         emp.calculating_wage()
     except Exception as e:
         logging.warning("Warning ..")
