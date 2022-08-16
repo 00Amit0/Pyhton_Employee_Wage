023cd73ea47f62ff7692f2b782a3cbc8481ab39b
“00Amit0” <amit836961@gmail.com>
2022-08-16 11:30:01 +0530

    Added wage

--- Emp_Wage.py
+++ Emp_Wage.py
@@ -13,10 +13,16 @@ class EmployeeWage:
         logging.debug("Employee wage running")
         try:
             check = random.randint(0, 1)
+            wage_per_hour = 20
+            emp_wage = 0
             if check == 0:
                 print('{0} : Employee is Absent'.format(check))
+                emp_wage = wage_per_hour * 0
+                print('{0} : is employee\'s wage'.format(emp_wage))
             else:
                 print('{0} : Employee is Present'.format(check))
+                emp_wage = wage_per_hour * 1
+                print('{0} : is employee\'s wage'.format(emp_wage))
         except Exception as ex:
             print(ex)
 
